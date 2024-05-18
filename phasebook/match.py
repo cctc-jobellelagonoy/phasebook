19321628494abd58126f09a3920f9dc03567136d
nymeria-1 <mjflagonoy@tip.edu.ph>
2024-05-18 11:12:10 +0800

    optimize is_match function

--- phasebook/match.py
+++ phasebook/match.py
@@ -19,9 +19,17 @@ def match(match_id):
     return {"message": msg, "elapsedTime": end - start}, 200
 
 
+# def is_match(fave_numbers_1, fave_numbers_2):
+#     for number in fave_numbers_2:
+#         if number not in fave_numbers_1:
+#             return False
+
+#     return True
+
 def is_match(fave_numbers_1, fave_numbers_2):
+    fave_numbers_1_set = set(fave_numbers_1)
     for number in fave_numbers_2:
-        if number not in fave_numbers_1:
+        if number not in fave_numbers_1_set:
             return False
 
     return True
